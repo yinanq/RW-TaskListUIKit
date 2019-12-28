e79e3cf827f5cfe8a8e2282e9d9c5efff70e522f
yinanq <yinancode@gmail.com>
2019-12-27 22:43:04 -0800

    started with an empty table view controller

---
+++ RW-TaskListUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RW-TaskListUIKit
+//
+//  Created by Yinan Qiu on 12/27/19.
+//  Copyright © 2019 Yinan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
